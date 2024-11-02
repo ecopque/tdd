c26e84e7ce4810a34fc44333d2eeed71eab55bcd
Edson Copque <ecop@disroot.org>
2024-11-02 18:09:21 -0300

    Assertions

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 # FILE: /TDD/calculator.py
 
 def sum_sum(x, y):
+    assert isinstance(x, (int, float)), 'X has to be INT or FLOAT, Edson.'
     return x + y
 
 
--- testing.py
+++ testing.py
@@ -6,11 +6,19 @@ from calculator import sum_sum
 # print(sum_sum(-10, 50))
 # print(sum_sum(10.5, 11))
 
+# try:
+#     print(sum_sum('100', 50))
+# except TypeError as error_error:
+#     print('Error, man!')
+#     print(error_error)
+
 try:
-    print(sum_sum('100', 50))
-except TypeError as error_error:
-    print('Error, man!')
-    print(error_error)
-    
+    print(sum_sum('10', 10))
+except AssertionError as error_error:
+    print(f'You screwed up, friend: {error_error}')
+
+
+
+#1: Response: 
 
 # https://linktr.ee/edsoncopque
