6c5f693ee93d3b3ed3fbdb15f4d2db95f3aded37
Edson Copque <ecop@disroot.org>
2024-11-02 18:20:51 -0300

    Assertions

--- calculator.py
+++ calculator.py
@@ -1,8 +1,11 @@
 # FILE: /TDD/calculator.py
 
 def sum_sum(x, y):
-    assert isinstance(x, (int, float)), 'X has to be INT or FLOAT, Edson.'
+    assert isinstance(x, (int, float)), 'X has to be INT or FLOAT, Edson.' #1: #2:
     return x + y
 
+
+#1: Essa linha utiliza uma assertiva (assert) para verificar se o valor de x é do tipo int ou float.O objetivo é garantir que x seja um número (inteiro ou ponto flutuante) antes de prosseguir com a operação de soma. Se x não for um int ou float, o código lança um AssertionError com a mensagem 'X has to be INT or FLOAT, Edson.'. A assertiva, neste caso, é uma medida de segurança para evitar a soma de tipos inadequados, como strings ou listas, que poderiam causar erros.
+#2: No contexto do módulo, isso é especialmente relevante porque o sum_sum será chamado por outro módulo (testing.py). Essa verificação ajuda a manter o código mais robusto e previne que entradas inválidas causem problemas durante a execução.
 
 # https://linktr.ee/edsoncopque
--- testing.py
+++ testing.py
@@ -12,13 +12,14 @@ from calculator import sum_sum
 #     print('Error, man!')
 #     print(error_error)
 
-try:
+try: #2:
     print(sum_sum('10', 10))
-except AssertionError as error_error:
+except AssertionError as error_error: #1: #3:
     print(f'You screwed up, friend: {error_error}')
 
 
-
-#1: Response: 
+#1: Response: You screwed up, friend: X has to be INT or FLOAT, Edson.
+#2: A vantagem é que usando 'assert' estamos nos comunicando com outros desenvolvedores, enquanto 'try'/'except' vai impedir que meu código seja interrompido;
+#3: Esta linha captura o AssertionError caso ele seja gerado ao tentar executar sum_sum('10', 10).
 
 # https://linktr.ee/edsoncopque
