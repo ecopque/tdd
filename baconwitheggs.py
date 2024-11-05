cb00b4f759a02feedd8f01dfcea9c71b15bc0387
Edson Copque <ecop@disroot.org>
2024-11-05 18:25:25 -0300

    Unittest with TDD

--- baconwitheggs.py
+++ baconwitheggs.py
@@ -14,14 +14,14 @@
 def bacon_with_eggs(n):
     assert isinstance(n, int), '"n" must be int.' #1: ##
 
-    if n % 3 == 0 and n % 5 == 0: #2:
-        return 'Bacon with eggs' #2:
+    if n % 3 == 0 and n % 5 == 0: #2: ##
+        return 'Bacon with eggs' #2: ##
     
     if n % 3 == 0: #4:
         return 'Bacon' #4:
     
-    if n % 5 == 0:
-        return 'Eggs'
+    if n % 5 == 0: #5:
+        return 'Eggs' #5:
     
     return 'To starve' #3:
 
@@ -30,6 +30,6 @@ def bacon_with_eggs(n):
 #2: Etapa 2;
 #3: Etapa 3;
 #4: Etapa 4;
-#5: 
+#5: Etapa 5;
 
 # https://linktr.ee/edsoncopque
