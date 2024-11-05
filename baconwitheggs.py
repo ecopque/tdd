a363ef4656f9758846b802b385c23d66d221ab54
Edson Copque <ecop@disroot.org>
2024-11-05 15:04:10 -0300

    Unittest with TDD

---
+++ baconwitheggs.py
@@ -0,0 +1,14 @@
+# FILE: /TDD/baconwitheggs.py
+'''
+1. Receber um número inteiro;
+2. Saber se o número é múltiplo de 3 e 5:
+    Bacon com ovos
+3. Saber se o número é múltiplo somente de 3:
+    Bacon
+4. Saber se o número é múltiplo somente de 5:
+    Ovos
+5. Saber se o número NÃO É múltiplo de 3 e 5:
+    Passar fome
+'''
+
+# https://linktr.ee/edsoncopque
