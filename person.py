b584d22ed84c3ad548ecd9a6e37b5ce128cb0fbf
Edson Copque <ecop@disroot.org>
2024-11-05 19:54:55 -0300

    Unittest with TDD

--- person.py
+++ person.py
@@ -4,6 +4,7 @@ class Person:
     def __init__(self, name, last_name):
         self.name = name
         self.last_name = last_name
+        self.data_obtained = False
 
 
 # https://linktr.ee/edsoncopque
--- test_person.py
+++ test_person.py
@@ -5,14 +5,23 @@ from person import Person
 
 
 class TestPerson(unittest.TestCase):
-    def setUp(self):
+    def setUp(self): ##
         self.p1 = Person('Edson', 'Copque')
 
     def test_person_attr_name_has_the_correct_value(self):
-        self.assertEqual(self.p1.name, 'Edson')
+        self.assertEqual(self.p1.name, 'Edson') ##
+
+    def test_person_attr_name_is_str(self):
+        self.assertIsInstance(self.p1.name, str) ##
 
     def test_person_attr_lastname_has_the_correct_value(self):
         self.assertEqual(self.p1.last_name, 'Copque')
+
+    def test_person_attr_lastname_is_str(self):
+        self.assertIsInstance(self.p1.last_name, str)
+
+    def test_person_attr_data_obtained_starts_false(self):
+        self.assertFalse(self.p1.data_obtained)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
