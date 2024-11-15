0ffc6ee21a3d4932ca675ed302d6412da8c08214
Edson Copque <ecop@disroot.org>
2024-11-15 15:51:02 -0300

    Type hints and MyPy

--- typehints1.py
+++ typehints1.py
@@ -1,11 +1,12 @@
 # FILE: /TDD/typehints1.py
 
 # pip install mypy
-from typing import List, Union #A:
+from typing import List, Union, Tuple #A: #B: #C:
+#A: List | #B: Union | #C: Tuple
 
 # primitive
 number: int = 10
-number: int = '10' # wrong
+number: int = '10' #wrong
 
 float_number: float = 10.5
 bool_number: bool = True
@@ -15,12 +16,17 @@ string_var: str = 'Hi'
 list_var: list = [1, 2, 3]
 list_var: List = [1, 2, 3] #A: ##
 list_var: List[int] = [1, 2, 3] #A: ##
-list_var: List[int] = [1, 2, 3, 'z'] #A: ## # wrong
+list_var: List[int] = [1, 2, 3, 'z'] #A: ## #wrong
 
 list_str_int: List[Union[int, str]] = [1, 2, 3, 'z'] #A: #B: ##
 
-tuple_var: tuple = [1, 2, 3] # wrong
+tuple_var: tuple = [1, 2, 3] #wrong
 tuple_var: tuple = (1, 2, 3)
 
+tuple_var: Tuple = (1, 2, 3) #C:
+tuple_var: Tuple[int] = (1, 2, 3) #C: #1: ## #wrong
+
+
+#1: No caso das tuplas, se tenho que informar que cada um é 'int'. Ver outro exemplo;
 
 # https://linktr.ee/edsoncopque
